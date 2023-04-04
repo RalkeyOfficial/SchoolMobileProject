93744aa1641d08e85b36d517d1791a2560a26c98
Thijn <Ralkey@outlook.com>
2023-04-04 12:59:10 +0200

    first test

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react"
+import { StyleSheet, Text, View, Button } from "react-native"
 
 export default function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{count}</Text>
+      <View style={styles.buttons}>
+        <Button title="+10" onPress={() => setCount(count + 10)} />
+        <Button title="+1" onPress={() => setCount(count + 1)} />
+        <Button title="reset" onPress={() => setCount(0)} />
+        <Button title="-1" onPress={() => setCount(count - 1)} />
+        <Button title="-10" onPress={() => setCount(count - 10)} />
+      </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 16,
   },
-});
+  buttons: {
+    flexDirection: "row",
+    gap: 16,
+  },
+})
